3f1b0b9cec8b16b4dd32043a06c72d9f9e99581d
moody-Jazz <102924428+moody-Jazz@users.noreply.github.com>
2025-05-18 20:50:50 +0530

    Add instruction set

--- chip8.cpp
+++ chip8.cpp
@@ -1,7 +1,35 @@
 #include "chip8.hpp"
 
-Chip8::Chip8() = default;
+Chip8::Chip8(){
+    prgrmCntr_ = STARTING_ADDRESS;
+    loadFontset();
+} 
+
 Chip8::~Chip8() = default;
+
+const uint8_t Chip8::FONTSET[80] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6 
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8 
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+void Chip8::loadFontset(){
+    for(int i{}; i<80; i++)
+        memory_[FONTSET_STARTING + i] = FONTSET[i];
+}
 
 void Chip8::loadRom(const std::string filename){
     std::ifstream file;
@@ -27,4 +55,110 @@ void Chip8::loadRom(const std::string filename){
         memory_[STARTING_ADDRESS + i] = buffer[i];
         
     file.close();
+}
+
+void Chip8::CLS(){
+    
+}
+
+void Chip8::RET(){
+}
+
+void Chip8::SYS_addr(){
+}
+
+void Chip8::JP_addr(){
+}
+
+void Chip8::CALL_addr(){
+}
+
+void Chip8::SE_Vx(){
+}
+
+void Chip8::SNE_Vx(){
+}
+
+void Chip8::SE_Vx_Vy(){
+}
+
+void Chip8::LD_Vx(){
+}
+
+void Chip8::ADD_Vx(){
+}
+
+void Chip8::LD_Vx_Vy(){
+}
+
+void Chip8::OR_Vx_Vy(){
+}
+
+void Chip8::AND_Vx_Vy(){
+}
+
+void Chip8::XOR_Vx_Vy(){
+}
+
+void Chip8::ADD_Vx_Vy(){
+}
+
+void Chip8::SUB_Vx_Vy(){
+}
+
+void Chip8::SHR_Vx(){
+}
+
+void Chip8::SUBN_Vx_Vy(){
+}
+
+void Chip8::SHL_Vx(){
+}
+
+void Chip8::SNE_Vx_Vy(){
+}
+
+void Chip8::LD_I(){
+}
+
+void Chip8::JP_V0(){
+}
+
+void Chip8::RND_Vx(){
+}
+
+void Chip8::DRW_Vx_Vy(){
+}
+
+void Chip8::SKP_Vx(){
+}
+
+void Chip8::SKNP_Vx(){
+}
+
+void Chip8::LD_Vx_DT(){
+}
+
+void Chip8::LD_Vx_K(){
+}
+
+void Chip8::LD_DT_Vx(){
+}
+
+void Chip8::LD_ST_Vx(){
+}
+
+void Chip8::ADD_I_Vx(){
+}
+
+void Chip8::LD_F_Vx(){
+}
+
+void Chip8::LD_B_Vx(){
+}
+
+void Chip8::LD_I_Vx(){
+}
+
+void Chip8::LD_Vx_I(){
 }
--- chip8.hpp
+++ chip8.hpp
@@ -8,18 +8,63 @@
 #include "iomanip"
 
 #define STARTING_ADDRESS 0x200
+#define FONTSET_STARTING 0x050
 
 class Chip8{
 public:
     Chip8();
     ~Chip8();
+    static const uint8_t FONTSET[80];
+    
+    void loadFontset();
     void loadRom(const std::string fileName);
+
+    // instruction set
+
+    void CLS();         // clear the display
+    void RET();         // return from a subroutine
+    void SYS_addr();    // jump to a machine code routine at nnn
+    void JP_addr();     // jump to location nnn
+    void CALL_addr();   // call subroutine at nnn
+    void SE_Vx();       // skip next instruction if Vx = kk
+    void SNE_Vx();      // skip next instruction if Vx != kk
+    void SE_Vx_Vy();    // skip next instruction if Vx = Vy
+    void LD_Vx();       // set Vx = kk
+    void ADD_Vx();      // set Vx = Vx + kk
+    void LD_Vx_Vy();    // set Vx = Vy
+    void OR_Vx_Vy();    // set Vx = Vx OR Vy
+    void AND_Vx_Vy();   // set Vx = Vx AND Vy
+    void XOR_Vx_Vy();   // set Vx = Vx XOR Vy
+    void ADD_Vx_Vy();   // set Vx = Vx + Vy, set VF = carry
+    void SUB_Vx_Vy();   // set Vx = Vx - Vy, set VF = NOT borrow
+    void SHR_Vx();      // set Vx = Vx SHR 1
+    void SUBN_Vx_Vy();  // set Vx = Vy - Vx, set VF = NOT borrow
+    void SHL_Vx();      // set Vx = Vx SHL 1
+    void SNE_Vx_Vy();   // skip next instruciton if Vx != Vy
+    void LD_I();        // set I = nnn
+    void JP_V0();       // jump to location nnn + V0
+    void RND_Vx();      // set Vx = random byte AND kk
+    void DRW_Vx_Vy();   // display n-byte sprite starting at mem location I at (Vx, Vy), set Vf = collision
+    void SKP_Vx();      // skip next instruction if key with the value of Vx is pressed
+    void SKNP_Vx();     // skip next instruction if key with the value of Vx is not pressed
+    void LD_Vx_DT();    // set Vx = delay timer value
+    void LD_Vx_K();     // wait for a key press, store the value of the key in Vx
+    void LD_DT_Vx();    // set delay timer = Vx
+    void LD_ST_Vx();    // set sound timer = Vx
+    void ADD_I_Vx();    // set I = I + Vx
+    void LD_F_Vx();     // set I = location of sprite for digit Vx
+    void LD_B_Vx();     // store BCD representation of Vx in mem location I, I+1 and I+2
+    void LD_I_Vx();     // store registers V0 throgh Vx in memory, starting at location I
+    void LD_Vx_I();     // Read registers V0 through Vx from memory starting at location I.
     
 private:
-    uint8_t  regV_[16];
-    uint16_t indexReg_;
-    uint16_t prgrmCntr_;
-    uint8_t  stkPtr_;
+    uint8_t  regV_[16]{};
+    uint16_t indexReg_{};
+    uint16_t prgrmCntr_{};
+    uint8_t  stkPtr_{};
 
-    uint8_t memory_[4096];
+    uint8_t memory_[4096]{};
+    uint16_t stack_[16]{};
+    uint8_t keypad_[16]{};
+    uint32_t displayBuffer[64 * 32]{};
 };
