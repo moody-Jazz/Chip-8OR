af4354a9c2b4ec1285bde6149e880dcc9ad07f59
moody-Jazz <102924428+moody-Jazz@users.noreply.github.com>
2025-05-25 00:04:22 +0530

    ADD main entry point

--- main.cpp
+++ main.cpp
@@ -1,8 +1,50 @@
 #include "chip8.hpp"
+#include "interface.hpp"
 
-int main()
-{
-    Chip8 cpu;
-    cpu.loadRom("IBM.ch8");
+int main(int argc, char* argv[]) {
+    int cyclesPerFrame = 10;
+    
+    Chip8 chip8;
+    Interface interface;
+    std::string rom;
+    
+    // the function below returns 1 if an error occured
+    if(interface.handleCmdArgs(argc, argv, cyclesPerFrame, rom)) return 1;
+    
+    chip8.loadRom(rom);
+    
+    InitWindow(SCREEN_WIDTH * interface.scale, SCREEN_HEIGHT * interface.scale, "CHIP-8 Emulator");
+    SetTargetFPS(60);
+    InitAudioDevice();
+
+    Sound beep = LoadSound("beep.wav");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        interface.inputHandler(chip8.keypad);
+
+        if(interface.restart){
+            chip8.restartProgram();
+            interface.restart = false;
+        }
+
+        if(!interface.pause)
+            for (int i = 0; i < cyclesPerFrame; ++i) 
+                chip8.cycle();
+
+        if(chip8.soundTimer == 1)
+            PlaySound(beep);
+
+        chip8.decrementTimers();
+        interface.updateFrame(chip8.displayBuffer, SCREEN_HEIGHT, SCREEN_WIDTH);
+
+        EndDrawing();
+    }
+    
+    UnloadSound(beep);
+    CloseAudioDevice();
+    CloseWindow();
     return 0;
 }
